8ff107d05b6c020eb60db65816670b5de114977b
BeatrizFava <00001134787753sp@al.educacao.sp.gov.br>
2026-05-04 16:07:46 +0000

    v3

---
+++ quizz.py
@@ -0,0 +1,9 @@
+print("Seja Bem Vindo ao Quizz de Formula 1🏎️")
+print("Qual Piloto é conhecido como Rei De Mônaco? /n A-Lewis Hamilton /n B-Ayrton Senna /n C-Michael Shumacher")
+if resposta ="B":
+  print("Você acertou, Parabéns!✅")
+else:
+  print("Você errou, Tente Novamente!❌")
+print("2-Qual equipe é conhecida pela cor vermelha tradicional? /n A-Ferrari /n B-Mercedes /n C-McLaren")
+if resposta_2 ="A"
+  
