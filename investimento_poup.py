18fcd06f4c1dd9cb3e3d09428fde034d7c0367c8
BeatrizFava <00001134787753sp@al.educacao.sp.gov.br>
2026-05-18 16:05:37 +0000

    v1

---
+++ investimento_poup.py
@@ -0,0 +1,10 @@
+#Simulador de Investimento - Poupança--
+deposito = float(input("Digite o valor do Aporte"))
+taxa = float(input("Qual a taxa da Poupança em % ? "))
+meses = int(input("Quantos meses vai investir?"))
+conversão = taxa/100
+total = 0 
+for mes in range(1,meses+1):
+    total = total + deposito
+    total = total + (total*taxa)
+print(f"Ao final do período, você terá: R${total:.2f}")
